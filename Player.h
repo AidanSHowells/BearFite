9b3943d92740754de7b6e402fd7cb2854184e424
AidanSHowells <36609740+AidanSHowells@users.noreply.github.com>
2018-03-01 19:23:47 -0500

    Declared TakeAction Function
    As part of this, LegPunch became private. Also removed extra include statement and added some white space

--- Player.h
+++ Player.h
@@ -2,19 +2,19 @@
 #define PLAYER_H
 
 #include "HUD.h"
-#include <string>//For std::to_string
 
 class Bear;
+
 
 class Player{
   public:
     Player(MessageBox& theMessages);
     int AC(); //Combines all AC-affecting factors
-    void LegPunch(Bear&);
     void Hurt(int); //How the bear injures the player
+    void TakeAction(Action theAction, Bear& theBear);//See below for syntax
   private:
     MessageBox* Messages; //So damage statements know where to print
-    int abil[6] = {10, 10, 10, 10, 10 ,10}; //STR, DEX, CON, INT, WIS, CHR
+    int abil[6] = {10, 10, 10, 10, 10 , 10}; //STR, DEX, CON, INT, WIS, CHR
     int maxHealth = 40;
     int health = maxHealth;
     int BaseAttackBonus = 0; //This will be level-based
@@ -22,6 +22,15 @@ class Player{
     int Armor = 0; //This will be determined by equipped items and magic
     int LegAttackBonus(); //Calculates attack bonus with abilities etc.
     int LegDamageBonus(); // Same for damage
+    void LegPunch(Bear& bear);
 };
+
+/*Syntax for TakeAction:
+if(event.type == sf::Event::MouseButtonPressed ||
+   event.type == sf::Event::KeyPressed)
+{
+  player.TakeAction(optionsBox.GetAction(event), bear);
+}
+*/
 
 #endif
