c82c0b452ec4276a9668f3b06b164574d30f8362
AidanSHowells <36609740+AidanSHowells@users.noreply.github.com>
2018-03-01 19:30:48 -0500

    Updated HUD.cpp
    Split off HUD.h, updated MessageBox::Update, added OptionsBox::GetAction, added OptionsBox::Highlight

--- HUD.cpp
+++ HUD.cpp
@@ -1,31 +1,8 @@
-#include <SFML/Graphics.hpp>
+#include "HUD.h"
 #include <string>//For std::to_string
 
-
-/*Eventually, there is going to be a base class "HUD" from which all other
- *classes in this file are derived.
- */
-
-/*The MessageBox class is for boxes displaying messages to the player. When
- *constructed, specify the window that the font displays in, the font the title
- *uses, the font everything else uses, and the title itself. Optionally, specify
- *the position and size of the box. Member functions are "Update", which takes a
- *string and makes it the first message displayed (shifting everything else down
- *one), and "draw", which draws the message box to the window.
- */
-
-/*The OptionsBox class is for listing the options available to the player. When
- *constructed, specify the window that the font displays in, the font the titles
- *use, and the font everything else uses. Optionally, specify the position and
- *size of the box, as well as the horizontal position of the divider line.
- *Member function is "draw", which draws the options box to the window.
- */
-
-
-
-
 /*Why force the person creating a HUD object to provide fonts? Two reasons.
- *First, constructors can't return values, so I can't tell the constructor to
+ *First, constructors can't return values, so we can't tell the constructor to
  *return an error code if the font fails to load. This makes it harder to fail
  *gracefully. Second, it's not as simple as loading fonts in the constructor
  *and assigning them to the text. From the "sf::Font" documentation: "It is
@@ -35,65 +12,21 @@
  *sf::Font instance for creating a text)."
  */
 
-
-//Note that if the message box has a width of 200, one can fit 22 characters
-
-
-//This should eventually end up in HUD.cpp (as opposed to in HUD.h)
-//I use upper case to be consistant with SFML
+//Color definitions. I use upper case to be consistant with SFML
 sf::Color Gray = sf::Color(192,192,192);
 
 
-class MessageBox{
-  public:
-    MessageBox(sf::RenderWindow& theWindow,
-               sf::Font& titleFont,
-               sf::Font& mainFont,
-               sf::String theTitle,
-               sf::Vector2f thePosition = sf::Vector2f(0,0),
-               sf::Vector2f theSize = sf::Vector2f(200, 400) );
-    //General note: The way to write a sf::Vector2f literal is sf::Vector2f(0,0)
+int numDigits(int number){
+  if(number == 0){return 1;}
 
-    ~MessageBox();
-    void Update(sf::String inputString);//With a width of 200, you can fit 22
-                                        //characters (counting leading '>')
-    void draw();//"Draw" would be consistant with our funcion naming convention,
-                //but "draw" is consistant with SFML
-
-  private:
-    sf::RenderWindow* window;
-    const sf::Vector2f position;
-    const sf::Vector2f size;
-    const int numLines;
-    sf::Text* line = new sf::Text[numLines];
-    sf::RectangleShape background;
-
-};
-
-
-class OptionsBox{
-  public:
-    OptionsBox(sf::RenderWindow& theWindow,
-               sf::Font& titleFont,
-               sf::Font& mainFont,
-               sf::Vector2f thePosition = sf::Vector2f(0,405),
-               sf::Vector2f theSize = sf::Vector2f(800, 195),
-               float dividerPosition = 420 );
-    void draw();//"Draw" would be consistant with our funcion naming convention,
-                //but "draw" is consistant with SFML
-
-  private:
-    sf::RenderWindow* window;
-    const sf::Vector2f position;
-    const sf::Vector2f size;
-    const float divPosition;
-    sf::RectangleShape background;
-    sf::RectangleShape divLine;
-    sf::Text punchTitle;
-    sf::Text punch[3];
-    sf::Text notPunchTitle;
-    sf::Text notPunch[3];
-};
+  int digits = 0;
+  if (number < 0){digits = 1;}//So the '-' counts as a digit
+  while (number) {
+    number /= 10;//Integer division ftw
+    digits++;
+  }
+  return digits;
+}
 
 
 MessageBox::MessageBox(
@@ -107,7 +40,7 @@ MessageBox::MessageBox(
   window(&theWindow),
   position(thePosition),
   size(theSize),
-  numLines(int(theSize.y) / 20)//My guess is that the number of lines of text
+  numLines(int(theSize.y / 20))//My guess is that the number of lines of text
   //displayed should be the integer part of size.y/20. Fix it if you disagree
 {
   //Make the background rectangle
@@ -139,9 +72,38 @@ void MessageBox::Update(sf::String inputString){
     line[i].setString(line[i-1].getString());
   }
   //Make the top line inputString
-  line[1].setString(inputString);
+  line[1].setString(sf::String(">") += inputString);
 }
 
+void MessageBox::Update(sf::String inputString1, sf::String inputString2){
+  //Move everything down one
+  for(int i = numLines - 1; i > 1; i--){
+    line[i].setString(line[i-1].getString());
+  }
+  //Make the top line inputString2
+  line[1].setString(sf::String("  ") += inputString2);
+
+  //Now move everything down one again and make the top line inputString1
+  Update(inputString1);
+}
+
+void MessageBox::Update(sf::String inputString, int imputInt){
+  //Move everything down one
+  for(int i = numLines - 1; i > 1; i--){
+    line[i].setString(line[i-1].getString());
+  }
+  //Make the top line inputInt
+  sf::String spacing = "";
+  for(int i = 0; i < 22 - numDigits(imputInt); i++){
+    spacing += sf::String(" ");
+  }
+  line[1].setString(spacing += sf::String(std::to_string(imputInt)));
+
+  //line[1].setString(sf::String("  ") += sf::String(std::to_string(imputInt)));
+
+  //Now move everything down one again and make the top line inputString
+  Update(inputString);
+}
 
 void MessageBox::draw(){
   window -> draw(background);//Recall that -> is a member access operator.
@@ -175,66 +137,143 @@ OptionsBox::OptionsBox(
   divLine.setPosition(divPosition, position.y);
 
   //Make the first title
-  punchTitle.setFont(titleFont);
-  punchTitle.setString("PUNCH:Where Punch Bear?");
-  punchTitle.setCharacterSize(30);
-  punchTitle.setFillColor(sf::Color::Black);
-  punchTitle.setPosition(position.x, position.y);
+  punch[0].setFont(titleFont);
+  punch[0].setString("PUNCH:Where Punch Bear?");
+  punch[0].setCharacterSize(30);
+  punch[0].setFillColor(sf::Color::Black);
+  punch[0].setPosition(position.x, position.y);
 
   //Make the first set of options
-  for(int i = 0; i < 3; i++){
+  for(int i = 1; i < 4; i++){
     punch[i].setFont(mainFont);
     punch[i].setCharacterSize(30);
     punch[i].setFillColor(sf::Color::Black);
-    punch[i].setPosition(position.x, position.y + 30 * (i + 1) );
+    punch[i].setPosition(position.x, position.y + 40 * i);
   }
-  punch[0].setString("1:Leg");
-  punch[1].setString("2:Eye");
-  punch[2].setString("3:John Hopkins");
+  punch[1].setString("1:Leg");
+  punch[2].setString("2:Eye");
+  punch[3].setString("3:John Hopkins");
 
   //Make the second title
-  notPunchTitle.setFont(titleFont);
-  notPunchTitle.setString("NOT PUNCH:What Do?");
-  notPunchTitle.setCharacterSize(30);
-  notPunchTitle.setFillColor(sf::Color::Black);
-  notPunchTitle.setPosition(divPosition + 10, position.y);
+  notPunch[0].setFont(titleFont);
+  notPunch[0].setString("NOT PUNCH:What Do?");
+  notPunch[0].setCharacterSize(30);
+  notPunch[0].setFillColor(sf::Color::Black);
+  notPunch[0].setPosition(divPosition + 10, position.y);
 
   //Make the second set of options
-  for(int i = 0; i < 3; i++){
+  for(int i = 1; i < 4; i++){
     notPunch[i].setFont(mainFont);
     notPunch[i].setCharacterSize(30);
     notPunch[i].setFillColor(sf::Color::Black);
-    notPunch[i].setPosition(divPosition + 10, position.y + 30 * (i + 1) );
+    notPunch[i].setPosition(divPosition + 10, position.y + 40 * i);
   }
-  notPunch[0].setString("4:Quaff Drank");
-  notPunch[1].setString("5:Cast Spell");
-  notPunch[2].setString("6:Flee");
+  notPunch[1].setString("4:Quaff Drank");
+  notPunch[2].setString("5:Cast Spell");
+  notPunch[3].setString("6:Flee");
+
+  //Make the first set of bounding boxes
+  for(int i = 0; i < 3; i++){
+    textBox[i] = punch[i + 1].getGlobalBounds();
+  }
+
+  //Make the second set of bounding boxes
+  for(int i = 3; i < 6; i++){
+    textBox[i] = notPunch[i - 2].getGlobalBounds();
+  }
 }
 
 void OptionsBox::draw(){
   //Recall that a->b is equivalent to (*a).b
   window -> draw(background);
   window -> draw(divLine);
-  window -> draw(punchTitle);
-  for(int i = 0; i < 3; i++){
+  for(int i = 0; i < 4; i++){
     window -> draw(punch[i]);
   }
-  window -> draw(notPunchTitle);
-  for(int i = 0; i < 3; i++){
+  for(int i = 0; i < 4; i++){
     window -> draw(notPunch[i]);
   }
 }
 
+Action OptionsBox::GetAction(sf::Event theEvent){
+  if(theEvent.type == sf::Event::KeyPressed){
+    if (theEvent.key.code == sf::Keyboard::Num1 ||
+        theEvent.key.code == sf::Keyboard::Numpad1)
+    {return Action::leg;}
 
-//The main function is temporary; it makes testing features easier
+    else if (theEvent.key.code == sf::Keyboard::Num2 ||
+             theEvent.key.code == sf::Keyboard::Numpad2)
+    {return Action::eye;}
+
+    else if (theEvent.key.code == sf::Keyboard::Num3 ||
+             theEvent.key.code == sf::Keyboard::Numpad3)
+    {return Action::john_hopkins;}
+
+    else if (theEvent.key.code == sf::Keyboard::Num4 ||
+             theEvent.key.code == sf::Keyboard::Numpad4)
+    {return Action::quaff;}
+
+    else if (theEvent.key.code == sf::Keyboard::Num5 ||
+             theEvent.key.code == sf::Keyboard::Numpad5)
+    {return Action::cast;}
+
+    else if (theEvent.key.code == sf::Keyboard::Num6 ||
+             theEvent.key.code == sf::Keyboard::Numpad6)
+    {return Action::flee;}
+
+    else{
+      return Action::nothing;
+    }
+  }
+  if(theEvent.type == sf::Event::MouseButtonPressed){
+    sf::Vector2f clickLocation(theEvent.mouseButton.x,theEvent.mouseButton.y);
+
+    if (textBox[0].contains(clickLocation)){
+      return Action::leg;
+    }
+    else if (textBox[1].contains(clickLocation)){
+      return Action::eye;
+    }
+    else if (textBox[2].contains(clickLocation)){
+      return Action::john_hopkins;
+    }
+    else if (textBox[3].contains(clickLocation)){
+      return Action::quaff;
+    }
+    else if (textBox[4].contains(clickLocation)){
+      return Action::cast;
+    }
+    else if (textBox[5].contains(clickLocation)){
+      return Action::flee;
+    }
+    else{
+      return Action::nothing;
+    }
+  }
+}
+
+void OptionsBox::Highlight(){
+  sf::RectangleShape highlight;
+  highlight.setFillColor(sf::Color(255,255,0,153));
+
+  for(int i = 0; i < 6; i++){
+    if(textBox[i].contains(sf::Vector2f(sf::Mouse::getPosition(*window)))){
+      highlight.setSize(sf::Vector2f(textBox[i].width,textBox[i].height));
+      highlight.setPosition(textBox[i].left,textBox[i].top);
+      window -> draw(highlight);
+    }
+  }
+
+}
+
+
+
+//The main function and these include statements are temporary; they make it
+//easier to test new features
+#include "Player.h"
+#include "Bear.h"
+
 int main(){
-
-  //Declare some strings
-  sf::String TEMP[4];
-  TEMP[0] = ">Carp, You miss.";
-  TEMP[1] = ">You got bear for 69";
-  TEMP[2] = ">Bear spare you.";
-  TEMP[3] = ">Bear bash you for 69";
 
 
   //Load the fonts
@@ -258,6 +297,10 @@ int main(){
   //Create the options box
   OptionsBox options(window,courierNewBd,courierNew);
 
+  //Make the player and bear
+  Player player(messages);
+  Bear bear(messages);
+
 
   while (window.isOpen()){
     sf::Event event;
@@ -265,25 +308,20 @@ int main(){
       if (event.type == sf::Event::Closed){
         window.close();
       }
-      if (event.type == sf::Event::KeyPressed){
-        if (event.key.code == sf::Keyboard::Num0 || event.key.code == sf::Keyboard::Numpad0){
-          messages.Update(TEMP[0]);
-        }
-        if (event.key.code == sf::Keyboard::Num1 || event.key.code == sf::Keyboard::Numpad1){
-          messages.Update(TEMP[1]);
-        }
-        if (event.key.code == sf::Keyboard::Num2 || event.key.code == sf::Keyboard::Numpad2){
-          messages.Update(TEMP[2]);
-        }
-        if (event.key.code == sf::Keyboard::Num3 || event.key.code == sf::Keyboard::Numpad3){
-          messages.Update(TEMP[3]);
-        }
+      if (event.type == sf::Event::KeyPressed ||
+          event.type == sf::Event::MouseButtonPressed)
+      {
+        player.TakeAction(options.GetAction(event), bear);
       }
+
     }
 
+
+    //Order is important here
     window.clear();
     messages.draw();
     options.draw();
+    options.Highlight();
     window.display();
   }
 
