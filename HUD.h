8478d0239f343a96528f1c4faf0d19438ea875ab
AidanSHowells <36609740+AidanSHowells@users.noreply.github.com>
2018-03-04 20:45:27 -0500

    Added the BearStats and Display classes

--- HUD.h
+++ HUD.h
@@ -3,16 +3,25 @@
 
 #include <SFML/Graphics.hpp>
 
-/*Eventually, there is going to be a base class "HUD" from which all other
- *classes in this file are derived.
+class Bear;
+class Player;
+
+/*The main class here (defined last for dependency reasons) is class Display.
+ *An object of this class contains one object corresponding to each piece of the
+ *HUD, and is responsible for managing these objects (plus any HUD
+ *functionality involving multiple of the objects) so that the the programmer
+ *doesn't have to corral all of the objects individually. Member function "draw"
+ *draws all member HUD objects to the window. Member function "Highlight"
+ *helps indicate to the user what options are available, and hightlights any
+ *clickable hitboxes that the user hovers over.
  */
 
 /*The MessageBox class is for boxes displaying messages to the player. When
  *constructed, specify the window that the font displays in, the font the title
  *uses, the font everything else uses, and the title itself. Optionally, specify
  *the position and size of the box. Member functions are "Update", which takes a
- *string and makes it the first message displayed (shifting everything else down
- *one), and "draw", which draws the message box to the window.
+ *string and makes it the first message displayed (more details below), and
+ *"draw", which draws the message box to the window.
  */
 
 /*The OptionsBox class is for listing the options available to the player. When
@@ -26,8 +35,24 @@
  *hovering over.
  */
 
+/*The PlayerStats class is for displaying the stats of the player. When
+ *constructed, specify the window that the font displays in, the font the title
+ *uses, the font everything else uses, and the player whose stats are being
+ *displayed. Optionally, specify the position and size of the box. //FINISH
+ *
+ */
+
+/*The BearStats class is for displaying the stats of the bear. When
+ *constructed, specify the window that the font displays in, the font the title
+ *uses, the font everything else uses, and the bear whose stats are being
+ *displayed. Optionally, specify the position and size of the box. Member
+ *function "draw" draws the object to the window. Member function "Update"
+ *updates the displayed stats of the bear. Note that as currently constructed
+ *(with default size) the bear's health can be at most 9,999,999.
+ */
 
 enum class Action {nothing, leg, eye, john_hopkins, quaff, cast, flee};
+
 
 
 //Note that if the message box has a width of 200, one can fit 22 characters
@@ -39,10 +64,8 @@ class MessageBox{
                sf::String theTitle,
                sf::Vector2f thePosition = sf::Vector2f(0,0),
                sf::Vector2f theSize = sf::Vector2f(200, 400) );
-    //General note: The way to write a sf::Vector2f literal is sf::Vector2f(0,0)
-
     ~MessageBox();
-    void Update(sf::String inputString);
+    void Update(sf::String inputString);//Details below
     void Update(sf::String inputString1, sf::String inputString2);
     void Update(sf::String inputString, int imputInt);
     void draw();//"Draw" would be consistant with our funcion naming convention,
@@ -55,8 +78,15 @@ class MessageBox{
     const int numLines;
     sf::Text* line = new sf::Text[numLines];
     sf::RectangleShape background;
-
 };
+
+ /*MessageBox::Update can take a single sf::String, add a '>' to the front, and
+  *make that sf::String the first thing displayed in the message box. Given extra
+  *arguments, it adds those to the dispay as well, each argument below the
+  *arguments listed before it. If the second argument is a sf::String, it get two
+  *spaces prepended. If it is an int, it is right aligned, and if it is a object
+  *of class Bear, the bear's name is displayed (right aligned).
+  */
 
 
 class OptionsBox{
@@ -66,12 +96,10 @@ class OptionsBox{
                sf::Font& mainFont,
                sf::Vector2f thePosition = sf::Vector2f(0,405),
                sf::Vector2f theSize = sf::Vector2f(800, 195),
-               float dividerPosition = 420 );
+               float dividerPosition = 420.0f );
     Action GetAction(sf::Event theEvent);
     void Highlight();
-    void draw();//"Draw" would be consistant with our funcion naming convention,
-                //but "draw" is consistant with SFML
-
+    void draw();//See comment in MessageBox
   private:
     sf::RenderWindow* window;
     const sf::Vector2f position;
@@ -82,6 +110,49 @@ class OptionsBox{
     sf::Text punch[4];
     sf::Text notPunch[4];
     sf::FloatRect textBox[6];
+};
+
+
+class BearStats{
+  public:
+    BearStats(sf::RenderWindow& theWindow,
+              sf::Font& titleFont,
+              sf::Font& mainFont,
+              Bear& theBear,
+              sf::Vector2f thePosition = sf::Vector2f(205, 0),
+              sf::Vector2f theSize = sf::Vector2f(390, 50) );
+    void Update();
+    void draw();//See comment in MessageBox
+  private:
+    sf::RenderWindow* window;
+    Bear* bear;
+    const sf::Vector2f position;
+    const sf::Vector2f size;
+    sf::RectangleShape background[3];
+    sf::Text bearInfo[6];
+};
+
+class Display{
+  public:
+    Display(sf::RenderWindow& theWindow,
+            sf::Font& titleFont,
+            sf::Font& mainFont,
+            Player& thePlayer,
+            Bear& theBear);
+    MessageBox messages;
+    OptionsBox options;
+    //PlayerStats playerStats;
+    BearStats bearStats;
+    void draw();
+    Action GetAction(sf::Event theEvent);
+    //void Highlight();//This should eventually be in charge of all highlighting
+
+  private:
+    sf::RenderWindow* window;
+    Player* player;
+    Bear* bear;
+    bool isPickingSpell = false;//The behavior of Hightlight changes based on
+                                //whether the user is picking spells or not
 };
 
 
