8b514d32499cebba30bff9aba6aafa2d34793035
AidanSHowells <36609740+AidanSHowells@users.noreply.github.com>
2018-03-04 20:36:22 -0500

    Added SetMessageBox and GetName functions
    I also got rid of the constructor, since SetMessageBox rendered it superfluous

--- Bear.cpp
+++ Bear.cpp
@@ -2,13 +2,17 @@
 #include "Bear.h"
 
 
-Bear::Bear(MessageBox& theMessages){Messages = &theMessages;}
+void Bear::SetMessageBox(MessageBox& theMessages){Messages = &theMessages;}
 
 int Bear::AttackBonus(){return (abil[0] - 10)/2;}
 
 int Bear::DamageBonus(){return (abil[0] - 10)/2;}
 
 int Bear::AC(){return BaseAC + Armor + (abil[1] - 10)/2;}
+
+sf::String Bear::GetName(){return sf::String("Kleptobear");}
+
+int Bear::GetHealth(){return health;}
 
 void Bear::Hurt(int dmg){health -= dmg;}
 
