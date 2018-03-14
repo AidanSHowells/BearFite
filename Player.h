8944ec44fab0bfb6372f2b0fd747aa95e7cac0d0
AidanSHowells <36609740+AidanSHowells@users.noreply.github.com>
2018-03-14 11:01:44 -0400

    Added GetHealth and Support For Taking Turns

--- Player.h
+++ Player.h
@@ -9,28 +9,33 @@ class Bear;
 class Player{
   public:
     void SetMessageBox(MessageBox& theMessages);
+    sf::String GetHealth();//Returns a string of the form "20/40" (w/out quotes)
+    int GetNumDranks();
+    //void Quaff();
     int AC(); //Combines all AC-affecting factors
     void Hurt(int); //How the bear injures the player
-    void TakeAction(Action theAction, Bear& theBear);//See below for syntax
+    bool TakeAction(Action theAction, Bear& theBear);//See below for syntax
   private:
     MessageBox* Messages; //So damage statements know where to print
     int abil[6] = {10, 10, 10, 10, 10 , 10}; //STR, DEX, CON, INT, WIS, CHR
     int maxHealth = 40;
     int health = maxHealth;
+    int numDranks = 5;
     int BaseAttackBonus = 0; //This will be level-based
     int BaseAC = 10; //This will be level-based
     int Armor = 0; //This will be determined by equipped items and magic
     int LegAttackBonus(); //Calculates attack bonus with abilities etc.
     int LegDamageBonus(); // Same for damage
-    void LegPunch(Bear& bear);
+    bool LegPunch(Bear& bear);
 };
 
 /*Syntax for TakeAction:
 if(event.type == sf::Event::MouseButtonPressed ||
    event.type == sf::Event::KeyPressed)
 {
-  player.TakeAction(optionsBox.GetAction(event), bear);
+  isPlayerTurn = player.TakeAction(optionsBox.GetAction(event), bear);
 }
+//Returns true if it's still the player's turn, false otherwise
 */
 
 #endif
