c62ca499a55e053f558b1f27a511c75b6ab006ae
Aidan Howells <Aidan.S.Howells@gmail.com>
2019-06-19 06:29:50 -0500

    Fixed bug in make.cmd
    Attempting to compile in a folder whose path contained spaces would fail

--- Make.cmd
+++ Make.cmd
@@ -2,7 +2,7 @@ g++ BattleTest.cpp Spell.cpp SpellList.cpp Bear.cpp Body.cpp BearList.cpp^
  Modifier.cpp BearBattle.cpp Player.cpp Feats.cpp RollDice.cpp^
  Display/HUD.cpp Display/BearStats.cpp Display/PlayerStats.cpp Display/Messages.cpp^
  Display/Options.cpp Display/Button.cpp Display/Highlight.cpp Display/AddSpace.cpp^
- -lsfml-graphics -lsfml-window -lsfml-system -iquote %cd% -Wall -fmax-errors=2^
+ -lsfml-graphics -lsfml-window -lsfml-system -iquote "%cd%" -Wall -fmax-errors=2^
  -o BattleTest.exe
 
 @IF %errorlevel% NEQ 0 (
@@ -13,7 +13,7 @@ g++ BearFite.cpp Spell.cpp SpellList.cpp Bear.cpp Body.cpp BearList.cpp^
  LevelUp.cpp Modifier.cpp BearBattle.cpp Player.cpp Feats.cpp RollDice.cpp^
  Display/HUD.cpp Display/BearStats.cpp Display/PlayerStats.cpp Display/Messages.cpp^
  Display/Options.cpp Display/Button.cpp Display/Highlight.cpp Display/AddSpace.cpp^
- -lsfml-graphics -lsfml-window -lsfml-system -iquote %cd% -Wall -fmax-errors=2^
+ -lsfml-graphics -lsfml-window -lsfml-system -iquote "%cd%" -Wall -fmax-errors=2^
  -o BearFite.exe
 
 @ECHO/
